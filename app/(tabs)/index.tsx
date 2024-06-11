8aa462364f5f97865bd6a7c9e6f40525abadf440
dudaroch <134696629+dudaroch@users.noreply.github.com>
2024-06-11 12:44:49 +0000

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,53 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#F2E7FF', dark: '#6B3F9F' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Bem Vindo!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Redes Sociais</ThemedText>
+        <ThemedText>
+        Quer se manter atualizado sobre todas as nossas novidades, eventos e conteúdos exclusivos?
+        Não deixe de conferir a página ao lado, onde você encontrará os botões que te conectarão às nossas redes sociais.
+        Siga-nos para não perder nada do que está por vir e fazer parte da nossa comunidade virtual. Estamos esperando por você do outro lado!
+        </ThemedText>
+      </ThemedView>
+      
+      
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ constants/Colors.ts
@@ -0,0 +1,26 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#6B3F9F';
+const tintColorDark = '#6B3F9F';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#6B3F9F',
+    tabIconDefault: '##6B3F9F',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+  },
+};
